f2adeba8dfbe453f4a41f01bc1177cd6e14ebfbf
Koen Romers <info@koenromers.com>
2016-04-28 15:39:27 +0200

    fix(service): change defining of methods

--- heartbeat.js
+++ heartbeat.js
@@ -3,52 +3,59 @@
    if(typeof angular !== 'undefined'){
      angular.module('ngCordova.plugins.heartbeat', [])
       .factory('$cordovaHeartbeat', ['$timeout', '$window', function ($q, $window) {
+
         var heartbeat;
-        return {
-          _init: function() {
-            heartbeat = $window.Heartbeat;
-            $timeout(function () {
-              heartbeat.on('bpm', function (bpm) {
-                $rootScope.$emit('$cordovaHeartbeat:bpm', bpm);
-              });
-              heartbeat.on('percentage', function (percentage) {
-                $rootScope.$emit('$cordovaHeartbeat:progress', percentage);
-              });
-              heartbeat.on('graph', function (graph) {
-                $rootScope.$emit('$cordovaHeartbeat:graphUpdate', graph);
-              });
-              heartbeat.on('status', function (status) {
-                $rootScope.$emit('$cordovaHeartbeat:statusChange', status);
-              });
-              heartbeat.on('warning', function (warning) {
-                $rootScope.$emit('$cordovaHeartbeat:warning', warning);
-              });
-              heartbeat.on('error', function (error) {
-                $rootScope.$emit('$cordovaHeartbeat:error', error);
-              });
-              heartbeat.on('hrv', function (hrv) {
-                $rootScope.$emit('$cordovaHeartbeat:hrv', hrv);
-              });
-            });
-          },
-          start: function() {
-            _init();
-            var q = $q.defer();
-            heartbeat.start();
-            q.resolve(heartbeat);
-            return q.promise;
-          },
-          stop: function() {
-            var q = $q.defer();
-            if (heartbeat === undefined) {
-              q.reject(new Error('start must be called before any other operation'));
-            } else {
-              heartbeat.stop();
-              q.resolve(heartbeat);
-            }
-            return q.promise;
-          }
+        var service = {
+          start: start,
+          stop: stop,
         };
+        return service;
+
+        function _init() {
+          heartbeat = $window.Heartbeat;
+          $timeout(function () {
+            heartbeat.on('bpm', function (bpm) {
+              $rootScope.$emit('$cordovaHeartbeat:bpm', bpm);
+            });
+            heartbeat.on('percentage', function (percentage) {
+              $rootScope.$emit('$cordovaHeartbeat:progress', percentage);
+            });
+            heartbeat.on('graph', function (graph) {
+              $rootScope.$emit('$cordovaHeartbeat:graphUpdate', graph);
+            });
+            heartbeat.on('status', function (status) {
+              $rootScope.$emit('$cordovaHeartbeat:statusChange', status);
+            });
+            heartbeat.on('warning', function (warning) {
+              $rootScope.$emit('$cordovaHeartbeat:warning', warning);
+            });
+            heartbeat.on('error', function (error) {
+              $rootScope.$emit('$cordovaHeartbeat:error', error);
+            });
+            heartbeat.on('hrv', function (hrv) {
+              $rootScope.$emit('$cordovaHeartbeat:hrv', hrv);
+            });
+          });
+        }
+
+        function start() {
+          _init();
+          var q = $q.defer();
+          heartbeat.start();
+          q.resolve(heartbeat);
+          return q.promise;
+        }
+        
+        function stop(){
+          var q = $q.defer();
+          if (heartbeat === undefined) {
+            q.reject(new Error('start must be called before any other operation'));
+          } else {
+            heartbeat.stop();
+            q.resolve(heartbeat);
+          }
+          return q.promise;
+        }
       }]);
      angular.module('ngCordova.plugins').requires.push('ngCordova.plugins.heartbeat');
    }
